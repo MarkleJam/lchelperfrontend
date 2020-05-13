450954f16c30264783442d2dee4cf54e3b55390d
Zhiquan Zhang <zzq20y@gmail.com>
2020-05-13 16:01:02 -0400

    Initialization

---
+++ leetcodehelper/SQL/CreateDBAndTable.sql
@@ -0,0 +1,21 @@
+create DATABASE Leetcodehelper;
+
+CREATE TABLE item (
+id INT  PRIMARY KEY, 
+name VARCHAR(255), 
+diff ENUM('easy', 'medium', 'hard'), 
+type1 VARCHAR(255), 
+type2 VARCHAR(255),
+type3 VARCHAR(255),
+grasp VARCHAR(255),
+lastaccessed date);
+
+select * from item;
+
+insert into item (id, name, diff, type1, type2, type3, grasp, lastaccessed) values(24, 'sample',
+'easy',
+'array',
+null,
+null,
+'almost',
+'2019-08-11');
