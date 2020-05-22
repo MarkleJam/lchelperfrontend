fb2dc6b7eef3406bf915e636c9de3e192df9fd98
Zhiquan Zhang <zzq20y@gmail.com>
2020-05-22 15:14:55 -0400

    History component

--- src/components/HistoryPage/History.jsx
+++ src/components/HistoryPage/History.jsx
@@ -1,17 +1,45 @@
 import React, {Component} from 'react';
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
-import config from '../config/dbconfig.js';
+import {Button, Form, FormGroup, Label, Input, Card, CardBody, Col} from 'reactstrap';
+import config from '../../config/dbconfig.js';
+import HistoryItem from './HistoryItem.jsx';
+import AddForm from './AddForm.jsx';
+
 const axios = require('axios');
 
-class Addnew extends Component {
+export default class History extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            problemid: this.props.match.params.id,
+            history: null,
+            date: null,
+            comment: null
         }
     }
 
-    render() {
+    options = {
+        method:'get',
+        url:config.ip + '',    
+      }
+    
+    async componentDidMount() {
+        this.options.method = 'get';
+        this.options.url = config.ip + '/history/' + this.state.problemid;
+        let ret = await axios(this.options);
+        this.setState({history:ret.data});    
+        //console.log(this.state.history)
+      }
 
+    render() {
+        if(this.state.history == null) return null;
+        return <div>
+            <h1>History of Problem No.{this.state.problemid}</h1>
+            {this.state.history.map(item => {
+                return <HistoryItem {...item} key = {item.id}></HistoryItem>
+            })}
+            <br/>
+            <br/>
+            <AddForm id={this.state.problemid}></AddForm>
+        </div>
     }
 }
