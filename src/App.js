b34eb1e41646a01aa351b051729dde7f4a249466
Zhiquan Zhang <zzq20y@gmail.com>
2020-05-25 15:13:33 -0400

    Add history route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //import './App.css';
 import Home from './components/Pages/Home.jsx';
 import History from './components/HistoryPage/History.jsx'
+import SearchResult from './components/Pages/SearchResult.jsx';
 
 class App extends Component {
 
@@ -12,6 +13,7 @@ class App extends Component {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/history/:id' component={History}/>
+        <Route exact path='/item/search' component={SearchResult}/>
       </Switch>
     </Router>
     )
