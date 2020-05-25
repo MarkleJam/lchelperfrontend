3b0b5b9be0feebcc5f4431edf82ec125f4daf28a
Zhiquan Zhang <zzq20y@gmail.com>
2020-05-25 15:25:32 -0400

    Add search on homepage

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import config from '../../config/dbconfig.js'
 import Table from '../Table.jsx';
 import ModelForm from '../Modal.jsx'
+import SearchModal from './SearchModal.jsx';
 import {Container} from 'reactstrap';
 
 const axios = require('axios');
@@ -10,7 +11,7 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: null
+            items: null,
           }     
     }
 
@@ -20,14 +21,13 @@ class Home extends Component {
     }
   
     async componentDidMount() {
-      this.options.url += '/item';
+      this.options.url = config.ip + '/item';
       let ret = await axios(this.options);
-      this.setState({items:ret});    
+      this.setState({items:ret});
     }
 
     render(){
       if(this.state.items === null) return null;
-
         return (
             <Container className="App">
               <div className="row">
@@ -44,6 +44,7 @@ class Home extends Component {
               </div> 
               <div className="row">
                   <ModelForm buttonLabel="Add">Hello!</ModelForm>
+                  <SearchModal></SearchModal>
               </div>     
             </Container>
           );
